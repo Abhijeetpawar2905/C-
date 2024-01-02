4abfbd71e6acf2af3b564a3e8c9109b03f32d7e8
Abhijeet Pawar <148451075+Abhijeetpawar2905@users.noreply.github.com>
2024-01-02 20:21:13 +0530

    Add files via upload

---
+++ 1_constr_distru.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+class class 
+{
+            int subject ;
+
+    public: 
+            
+            class()
+            {
+                subject =0;
+                cout<<"Inside parameterless constructor"<<end;
+            }
+
+            ~class()
+            {
+                cout<<"Inside Pa"
+            }
+
+};
